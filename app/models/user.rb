cf430fc2d1f240551d4e82a31f3c1d1ce1cd9048
Boots2291 <buteramv@gmail.com>
2017-11-02 15:01:01 -0600

    Add nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :user_roles
   has_many :roles, through: :user_roles
-  
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates :first_name, presence: true
