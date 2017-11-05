b978d8bddc2d2613e56f46c426c4fa866aa9ac70
Boots2291 <buteramv@gmail.com>
2017-11-05 11:54:25 -0700

    Add orders specs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,16 +7,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  def show
-    @order = Order.find(params[:id])
-    @order_items = @order.order_items
-    unless session[:user_id] == @order.user_id
-      flash[:notice] = "You don't have access to that order!"
-      redirect_to dashboard_path(session[:user_id])
-      return
-    end
-  end
-
   def new
 		order = Order.create(status: 0, user_id: current_user.id, total_price: @cart.cart_total)
 		item_hash = @cart.order_hash
