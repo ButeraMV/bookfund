f6c1e267206b7a2938da146641117c2df8dc6336
Boots2291 <buteramv@gmail.com>
2017-11-05 22:04:22 -0700

    Fix test that was broken by pagination

--- spec/features/ebooks/ebooks_can_be_viewed_spec.rb
+++ spec/features/ebooks/ebooks_can_be_viewed_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe 'Ebooks can be viewed' do
   it 'in an index' do
-    25.times do
+    10.times do
       create(:ebook)
     end
 
     visit ebooks_path
 
-    expect(page).to have_css(".ebook-index-item", count: 20)
-    expect(page).to have_css(".title", count: 20)
-    expect(page).to have_css(".description", count: 20)
+    expect(page).to have_css(".ebook-index-item", count: 8)
+    expect(page).to have_css(".title", count: 8)
+    expect(page).to have_css(".description", count: 8)
   end
 end
